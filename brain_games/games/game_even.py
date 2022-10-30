838820671a0ad749f614c85acb758ab9a582f75c
Anisimoff <happyprooo@gmail.com>
2022-10-30 14:21:58 +0300

    "brain-even" fix

--- brain_games/games/game_even.py
+++ brain_games/games/game_even.py
@@ -7,7 +7,7 @@ import prompt
 def even_game():
     name = game_logic.name
     number = randint(1,6)
-    answer = prompt.string(f"Answer \"yes\" if the number is even, otherwise answer \"no\"\nQuestion: {number}\n")
+    answer = prompt.string(f"Answer \"yes\" if the number is even, otherwise answer \"no\".\nQuestion: {number}\n")
     print("Your answer: ", answer)
 
     if answer == "yes" and number % 2 == 0 or answer == "no" and number % 2 != 0:
