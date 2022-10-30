6899f6f280f5d4a18faa5a3e9d1b89161a8215b3
Anisimoff <happyprooo@gmail.com>
2022-10-30 14:06:00 +0300

    fix brain-games

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import brain_games.cli as cli
 
-
+print(__name__)
 def main():
     print("Welcome to the Brain Games!")
     x = cli.welcome_user()
@@ -9,3 +9,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+else:
+    def main():
+        print("Welcome to the Brain Games!")
+        x = cli.welcome_user()
