8cb90d62ecb2f8467eb19f32542e3a4f29ba4ef6
Anisimoff <happyprooo@gmail.com>
2022-10-30 18:57:14 +0300

    brain-calc small fix

--- brain_games/games/game_calc.py
+++ brain_games/games/game_calc.py
@@ -8,7 +8,7 @@ def calc_game():
     number1 = randint(1,30)
     number2 = randint(1,30)
     action = choice(["+","-","*"])
-    print(f"What the result of the expression?\nQuestion: {number1} {action} {number2}")
+    print(f"What is the result of the expression?\nQuestion: {number1} {action} {number2}")
     answer = prompt.string("Your answer: ")
 
     if action == "+":
